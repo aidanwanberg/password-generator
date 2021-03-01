4249f037ff211ff7e221b5d6f9819fc640f9a4bf
Aidan Wanberg <aidanwanberg@gmail.com>
2021-03-01 13:32:00 -0800

    added final comments throughout

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-// These are the variables for the password criteria
+// These are the four different variables that are needed for the password criteria
 var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
@@ -10,7 +10,7 @@ var specialCharacters = "(~!@#$%^&*_'<>,.?/)-+=`|){}[]:;"
 var numbers = "0123456789"
 
 
-
+// This function called 'generatePassword' includes prompt and confirm functions that ask the user for password criteria
 function generatePassword() {
   var passwordLength = prompt("How many characters would you like your password to be? (pick a number between 8 and 128)")
   console.log(passwordLength)
@@ -27,12 +27,13 @@ function generatePassword() {
   var includeNumbers = confirm("Would you like to include numbers?")
   console.log(includeNumbers)
 
+  // This 'empty string' variable will soon be filled in with the various password criteria that the user selects
   var finalPassword = ""
   console.log(finalPassword)
 
 
-
-
+  // These 'if statements' determine whether or not the variables will be included in the password generation
+  // Basically, if the confirm functions read 'true', then the corresponding variable (uppercase letters, special characters, etc) will be added to the finalPassword string
   if (includeNumbers) {
     finalPassword += numbers
 
@@ -54,32 +55,33 @@ function generatePassword() {
   }
   console.log(finalPassword)
 
+
+  // This .split method converts the finalPassword string into an array, allowing the future for loop to loop through
   finalPassword.split("");
 
-var password = ""
+  // This variable will be returned if the password length doesn't meet the criteria
+  // It will be used to return an empty value so that the user cannot generate a password that is outside of the length criteria
+  var password = ""
 
-if (passwordLength < 8 || passwordLength > 128) {
+  // This if statement will alert the user that their password length does not meet the requirement
+  if (passwordLength < 8 || passwordLength > 128) {
   alert("Password length does not meet requirement")
   return password
-}
+  }
 
-
+  // This for loop will continue to loop until it meets the specified password length
   for (var i = 0; i < passwordLength; i++) {
     
-    
-
+    // This variable will generate a random number that is based on the length of the array 'finalPassword'
     var randomNumber = Math.floor(Math.random() * finalPassword.length)
     console.log(randomNumber)
     
+    // This effectively uses the randomNumber to generate a random value from the array finalPassword
+    // This will continue to loop until it reaches the password length criteria
     password += finalPassword[randomNumber]
   }
   console.log(password)
   return password
-
-  
-
-
-// newPassword = ""
 
 };
 
